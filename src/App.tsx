00a512a13c5f04988394a361cf04a26576559522
Vikas <vikas.kumar.singh1198@gmail.com>
2025-01-20 16:18:33 +0530

    Created Cofig for api

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,36 @@
 import './App.css'
-import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Layout from './components/Layout'
 import { ThemeProvider } from "@/components/context/theme-provider"
 import WeatherDashboard from './pages/weather-dashboard'
 import CityPage from './pages/city-page'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import {
+  useQuery,
+  useMutation,
+  useQueryClient,
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
 function App() {
 
+  const queryClient = new QueryClient()
 
   return (
-    <BrowserRouter>
-          <ThemeProvider  defaultTheme="dark">
-            <Layout>
-                <Routes>
-                    <Route path='/' element={<WeatherDashboard/>}/>
-                    <Route path='/city/:cityName' element={<CityPage/>}/> 
-                </Routes>
-            </Layout>
-          </ThemeProvider>
-    </BrowserRouter>
+
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <ThemeProvider defaultTheme="dark">
+          <Layout>
+            <Routes>
+              <Route path='/' element={<WeatherDashboard />} />
+              <Route path='/city/:cityName' element={<CityPage />} />
+            </Routes>
+          </Layout>
+        </ThemeProvider>
+      </BrowserRouter>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   )
 }
 
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,28 +1,35 @@
 import { Link } from "react-router-dom"
 import { useTheme } from "./context/theme-provider"
 import { Button } from "./ui/button";
+import { Moon, Sun } from "lucide-react";
 
 const Header = () => {
-    const{theme,setTheme} = useTheme();
+    const { theme, setTheme } = useTheme();
     const isDark = theme === "dark";
-  return (
-   <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur py-2 supports-[backdrop-filter]:bg-background/60">
-        <div className="container mx-auto flex h-16 items-center justify-between px-4">
-            <Link to={"/"}>
-                <img src={ isDark ? "/logo.png" : "/logo2.png"} alt="klimate-logo" className="h-14" />
-            </Link>
+    return (
+        <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur py-2 supports-[backdrop-filter]:bg-background/60">
+            <div className="container mx-auto flex h-16 items-center justify-between px-4">
+                <Link to={"/"}>
+                    <img src={isDark ? "/logo.png" : "/logo2.png"} alt="klimate-logo" className="h-14" />
+                </Link>
 
-            <div>
-            {/**Search Bar */}
-            {/**Theme */}
-            <div>
-                <Button variant={"outline"} onClick={()=>setTheme(isDark?"light":"dark")}>Toggle</Button>
+                <div>
+                    {/**Search Bar */}
+                    {/**Theme */}
+                    <div className={`flex items-center cursor-pointer transition-transform duration-500
+                        ${isDark ? "rotate-180" : "rotate-0"}
+                        
+                    `}
+                     onClick={() => setTheme(isDark ? "light" : "dark")}>
+                        {isDark ? (<Sun className="h-6 w-6 text-yellow-500 rotate-0 transition-all" ></Sun>) : (
+                            <Moon className="h-6 w-6 text-blue-500 rotate-0 transition-all"></Moon>
+                        )}
+                    </div>
+                </div>
             </div>
-        </div>
-        </div>
-        
-   </header>
-  )
+
+        </header>
+    )
 }
 
 export default Header
