d2413ab1089f54f884e94d1d65b76da0cbf799ab
Vikas <vikas.kumar.singh1198@gmail.com>
2025-01-21 16:06:33 +0530

    Finished building the Dashboard

--- src/pages/weather-dashboard.tsx
+++ src/pages/weather-dashboard.tsx
@@ -8,7 +8,11 @@ import {
   AlertDescription,
   AlertTitle,
 } from "@/components/ui/alert"
-import { useReverseGeocodeQuery } from "@/hooks/useWeather";
+import { useForecastQuery, useReverseGeocodeQuery, useWeatherQuery } from "@/hooks/useWeather";
+import CurrentWeather from "@/components/CurrentWeather";
+import HourlyTemprature from "@/components/HourlyTemprature";
+import WeatherDetails from "@/components/WeatherDetails";
+import WeatherForecast from "@/components/WeatherForecast";
 const WeatherDashboard = () => {
 
   const {coordinates,
@@ -19,7 +23,19 @@ const WeatherDashboard = () => {
 
 
   const locationQuery = useReverseGeocodeQuery(coordinates);
-  console.log(locationQuery);
+  const forecastQuery = useForecastQuery(coordinates);
+  const weatherQuery = useWeatherQuery(coordinates);
+
+
+  const handleRefresh = ()=>{
+    getLocation();
+
+    if(coordinates){
+      weatherQuery.refetch();
+      locationQuery.refetch();
+      forecastQuery.refetch();
+    }
+  }
 
   if(loadingLocation){
     return <WeatherSkeleton/>
@@ -55,6 +71,31 @@ const WeatherDashboard = () => {
     )
   }
 
+  const locationName = locationQuery.data?.[0];
+
+  if(weatherQuery.error || forecastQuery.error){
+    return (
+      <Alert variant="destructive">
+      <AlertTriangle className="h-4 w-4" />
+      <AlertTitle>Error</AlertTitle>
+      <AlertDescription>
+        <p>Failed to Fetch Weather Data Please Try Again</p>
+        <Button onClick={getLocation}>
+          <RefreshCw 
+          onClick={handleRefresh}
+           className="w-4 h-4 mr-2"/>
+          Retry
+        </Button>
+      </AlertDescription>
+    </Alert>
+    )
+  }
+
+  if(!weatherQuery.data || !forecastQuery.data){
+    return <WeatherSkeleton/>
+  }
+  
+
   return (
     <div className="space-y-4">
       {/**Favourite Cities */}
@@ -62,10 +103,28 @@ const WeatherDashboard = () => {
            <h1 className="text-xl font-bold tracking-tight">My Locations</h1>
            <Button variant={"outline"}
             size={"icon"}
-
+            onClick={handleRefresh}
+            disabled={weatherQuery.isFetching || forecastQuery.isFetching}
            >
-              <RefreshCw className="w-4 h-4"/>
+              <RefreshCw className={`h-4 w-4 ${weatherQuery.isFetching ? "animate-spin":""}`}/>
            </Button>
+       </div>
+
+
+       {/**Current and Hourly Weather*/}
+
+       <div className="grid gap-6">
+          <div className="flex flex-col lg:flex-row gap-2">
+            <CurrentWeather data={weatherQuery.data} locationName={locationName} />
+            <HourlyTemprature data={forecastQuery.data} />
+          </div>
+
+          <div className="grid gap-6 md:grid-cols-2 items-start">
+            {/*deatils*/}
+            <WeatherDetails data={weatherQuery.data}/>
+            {/** forecast */}
+            <WeatherForecast data={forecastQuery.data} />
+          </div>
        </div>
     </div>
   )
