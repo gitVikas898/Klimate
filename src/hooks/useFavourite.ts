964bbfee07aca333138a84d9ee8f4b1209c3e476
Vikas <vikas.kumar.singh1198@gmail.com>
2025-01-22 11:46:24 +0530

    wrote usefavorite hook to add,remove favorite city worked on city page

---
+++ src/hooks/useFavourite.ts
@@ -0,0 +1,73 @@
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { useLocalStorage } from "./useLocalStorage"
+
+
+interface FavouriteCity{
+    id:string;
+    lat:number;
+    lon:number;
+    country:string;
+    name:string;
+    state?:string;
+    addedAt:number;
+}
+
+export function useFavourite(){
+  const[favorites,setFavorites] =  useLocalStorage<FavouriteCity[]>("favorites",[])
+
+const queryClient = useQueryClient();
+
+ const favoriteQuery = useQuery({
+    queryKey:["favorites"],
+    queryFn :()=>favorites,
+    initialData : favorites,
+    staleTime:Infinity,
+  })
+
+  const addToFavorite = useMutation({
+
+    mutationFn:async (city : Omit<FavouriteCity,"id" | "addedAt">)=>{
+        const newFavorite:FavouriteCity = {
+            ...city,
+            id:`${city.lat}-${city.lon}-${Date.now()}`,
+            addedAt:Date.now(),
+        };
+
+       const exists = favorites.some((fav)=>fav.id === newFavorite.id);
+       if(exists) return favorites;
+
+
+        const newFavorites = [...favorites,newFavorite].slice(0,10);
+
+        setFavorites(newFavorites)
+        return newFavorite;
+    },
+
+    onSuccess:()=>{
+        queryClient.invalidateQueries({
+            queryKey:["favorites"],
+        });
+    },
+
+  })
+
+  const removeFavorite = useMutation({
+    mutationFn:async(cityId:string)=>{
+        const newFavorites = favorites.filter((city)=>city.id!==cityId);
+        setFavorites(newFavorites);
+        return newFavorites;
+    },
+
+    onSuccess:()=>{
+        queryClient.invalidateQueries({
+            queryKey:["favorites"],
+        });
+    }
+  });
+
+  return {
+   favorites:favoriteQuery.data,
+   addToFavorite,
+   removeFavorite,
+  }
+}
--- src/pages/city-page.tsx
+++ src/pages/city-page.tsx
@@ -1,8 +1,72 @@
+import CurrentWeather from "@/components/CurrentWeather";
+import HourlyTemprature from "@/components/HourlyTemprature";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import WeatherDetails from "@/components/WeatherDetails";
+import WeatherForecast from "@/components/WeatherForecast";
+import WeatherSkeleton from "@/components/WeatherSkeleton";
+import { useForecastQuery, useWeatherQuery } from "@/hooks/useWeather";
+import { AlertTriangle } from "lucide-react";
+import { useParams,useSearchParams } from "react-router-dom";
+
+
 const CityPage = () => {
+
+  const[searchParams] = useSearchParams();
+
+  const params = useParams();
+  const lat = parseFloat(searchParams.get("lat") || "0");
+  const lon = parseFloat(searchParams.get("lon") || "0");
+
+  const coordinates = {lat,lon};
+
+  const weatherQuery = useWeatherQuery(coordinates);
+  const forecastQuery = useForecastQuery(coordinates);
+
+
+  if(weatherQuery.error || forecastQuery.error){
+    return (
+      <Alert variant="destructive">
+      <AlertTriangle className="h-4 w-4" />
+      <AlertTitle>Error</AlertTitle>
+      <AlertDescription>
+        <p>Failed to Fetch Weather Data Please Try Again</p>
+      </AlertDescription>
+    </Alert>
+    )
+  }
+
+  if(!weatherQuery.data || !forecastQuery.data || !params.cityName){
+    return <WeatherSkeleton/>
+  }
+  
+
+
   return (
-    <div>
-        City Page
-    </div>
+    <div className="space-y-4">
+    {/**Favourite Cities */}
+     <div className="flex items-center justify-between">
+         <h1 className="text-xl font-bold tracking-tight">{params.cityName},{weatherQuery.data.sys.country}</h1>
+     </div>
+      <div>
+          {/* add to fav */}
+      </div>
+
+     {/**Current and Hourly Weather*/}
+
+     <div className="grid gap-6">
+        <div className="flex flex-col lg:flex-row gap-2">
+          <CurrentWeather data={weatherQuery.data} />
+          <HourlyTemprature data={forecastQuery.data} />
+        </div>
+
+        <div className="grid gap-6 md:grid-cols-1 items-start">
+          {/*deatils*/}
+          <WeatherDetails data={weatherQuery.data}/>
+          {/** forecast */}
+          <WeatherForecast data={forecastQuery.data} />
+        </div>
+     </div>
+  </div>
   )
 }
 
